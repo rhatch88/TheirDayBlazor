8b8051fe75e562eaf878e28d8b3c5692a0ddaa4f
Rebecca Hatch <rhatch88@gmail.com>
2025-03-06 17:42:48 -0500

    added filter

--- TheirDayBlazor/Models/Event.cs
+++ TheirDayBlazor/Models/Event.cs
@@ -7,12 +7,14 @@ namespace TheirDayBlazor.Models
     {
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [Required]
 
-        public string AgeGroup { get; set; }
-        [Column(TypeName = "decimal(10,2)")]    
-        public decimal Price { get; set; }
-        public string Location { get; set; }
-        public bool IsOutdoor { get; set; }    
+        public string AgeGroup { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; } = 0.0m;
+        [Required]
+        public string Location { get; set; } = string.Empty;
+        public bool IsOutdoor { get; set; } = false;
     }
 }
