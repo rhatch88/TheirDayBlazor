8c1aa88a387dc8e934c34aa8530d93fffc3a29e8
Rebecca Hatch <rhatch88@gmail.com>
2025-03-09 23:05:06 -0400

    3/9 updates

--- TheirDayBlazor/Migrations/20250307002934_UpdateDatabaseSchema.Designer.cs
+++ TheirDayBlazor/Migrations/20250307002934_UpdateDatabaseSchema.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheirDayBlazor.Data;
 
@@ -10,9 +11,11 @@ using TheirDayBlazor.Data;
 namespace TheirDayBlazor.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250307002934_UpdateDatabaseSchema")]
+    partial class UpdateDatabaseSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TheirDayBlazor/Migrations/20250307002934_UpdateDatabaseSchema.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheirDayBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsOutdoor",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsOutdoor",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
